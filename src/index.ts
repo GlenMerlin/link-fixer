35699024c8e63bbd308328f1321a80e2b4a52778
Thalia Archibald <thalia@archibald.dev>
2023-06-29 03:54:23 -0600

    Handle @username TikTok links

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,12 @@ client.on('messageCreate', message => {
 
 client.login(token);
 
-const twitterPattern = `https://twitter\.com/([a-zA-Z0-9_]{1,15}/status/[0-9]+)`;       // Capture 1
-const instagramPattern = `https://(?:www\.)?instagram\.com/((?:p|reel)/[a-zA-Z0-9_]+)`; // Capture 2
-const tiktokPattern = `https://(?:www\.)tiktok\.com/(t/[a-zA-Z0-9]{9})`;                // Capture 3
-const linkPattern = [twitterPattern, instagramPattern, tiktokPattern].join('|').replace(`/`, `\/`);
+const linkPattern = [
+    `https://(www\.)?twitter\.com/([a-zA-Z0-9_]{1,15}/status/[0-9]+)`, // Captures 1 and 2
+    `https://(www\.)?instagram\.com/((?:p|reel)/[a-zA-Z0-9_]+)`, // Captures 3 and 4
+    `https://(www\.)?tiktok\.com/(t/[a-zA-Z0-9]{9})`, // Captures 5 and 6
+    `https://(www\.)?tiktok\.com/(@[a-zA-Z0-9_.]*[a-zA-Z0-9_]/video/[0-9]+)`, // Captures 7 and 8
+].join('|').replace(`/`, `\/`);
 const linkRe = new RegExp(linkPattern, 'gi');
 
 function fixLinks(message: string): string[] {
@@ -38,12 +40,14 @@ function fixLinks(message: string): string[] {
         for (const match of matches) {
             const link = match[0];
             let fixed;
-            if (match[1] != null) {
-                fixed = `https://fxtwitter.com/${match[1]}`;
-            } else if (match[2] != null) {
-                fixed = `https://www.ddinstagram.com/${match[2]}`;
-            } else if (match[3] != null) {
-                fixed = `https://www.vxtiktok.com/${match[3]}`;
+            if (match[2] != null) {
+                fixed = `https://${match[1] || ''}fxtwitter.com/${match[2]}`;
+            } else if (match[4] != null) {
+                fixed = `https://${match[3] || ''}ddinstagram.com/${match[4]}`;
+            } else if (match[6] != null) {
+                fixed = `https://${match[5] || ''}vxtiktok.com/${match[6]}`;
+            } else if (match[8] != null) {
+                fixed = `https://${match[7] || ''}vxtiktok.com/${match[8]}`;
             } else {
                 console.error(`No pattern matched! ${JSON.stringify(message)}`);
                 continue;
