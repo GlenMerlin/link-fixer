6d099432dceb61ba51ef6de20453a6cdd02d6d27
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2023-09-14 18:32:32 -0600

    fix: Moar grammar

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,13 @@ import { assert, nonempty, string, type } from "superstruct";
 // Load config file
 const config = require('../.config.json') as unknown;
 
-// Make sure the config is the right shape
+// Define the config's expected shape
 const configSchema = type({
   clientID: nonempty(string()),
   token: nonempty(string())
 });
 
-// Fail early if config is not the expected shape
+// Fail early if config is malformed
 assert(config, configSchema);
 
 export const clientID: string = config.clientID;
